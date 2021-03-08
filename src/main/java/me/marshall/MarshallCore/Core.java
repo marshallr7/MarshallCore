e1a649f37214060d46b05d7d1581cd211c34baa8
Marshall Patterson <mryanp7@gmail.com>
2021-03-07 20:03:37 -0600

    Added vault economy check and created constructor

--- src/main/java/me/marshall/MarshallCore/Core.java
+++ src/main/java/me/marshall/MarshallCore/Core.java
@@ -3,7 +3,9 @@ package me.marshall.MarshallCore;
 import me.marshall.MarshallCore.MenuSystem.Commands.ExampleCommand;
 import me.marshall.MarshallCore.MenuSystem.Listeners.MenuListener;
 import me.marshall.MarshallCore.MenuSystem.PlayerMenuUtility;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -11,12 +13,33 @@ import java.util.HashMap;
 public final class Core extends JavaPlugin {
 
     private static final HashMap<Player, PlayerMenuUtility> playerMenuUtilityMap = new HashMap<>();
+    private static Economy econ = null;
+    private static Core plugin;
+
+    public static Core getInstance() {
+        return plugin;
+    }
+
+    public static Economy getEconomy() {
+        return econ;
+    }
+
+
 
     @Override
     public void onEnable() {
-        getCommand("examplecommand").setExecutor(new ExampleCommand());
+        plugin = this;
 
+        getCommand("examplecommand").setExecutor(new ExampleCommand());
         getServer().getPluginManager().registerEvents(new MenuListener(), this);
+
+
+        //Vault Check
+        if (!setupEconomy()) {
+            System.out.println("No economy plugin found. Disabling Vault.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 
     }
 
@@ -36,4 +59,18 @@ public final class Core extends JavaPlugin {
             return playerMenuUtility;
         }
     }
+
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return true;
+    }
+
 }
