94a23f19e5c30ace11597370915669d3f1df989f
Marshall Patterson <mryanp7@gmail.com>
2021-03-09 11:08:32 -0600

    Added puke code for later

--- src/main/java/me/marshall/MarshallCore/Bar/Listeners/BarListener.java
+++ src/main/java/me/marshall/MarshallCore/Bar/Listeners/BarListener.java
@@ -2,29 +2,35 @@ package me.marshall.MarshallCore.Bar.Listeners;
 
 import me.marshall.MarshallCore.Core;
 import net.md_5.bungee.api.ChatColor;
+import org.apache.commons.lang.mutable.MutableInt;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.Vector;
 
-import java.util.HashMap;
-import java.util.Objects;
-import java.util.Random;
-import java.util.UUID;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 
 public class BarListener implements Listener {
 
     public HashMap<UUID, Double> playerConsumption = new HashMap<>();
+    int count = 50;
+    private Random pukeRand;
+
 
     @EventHandler
     public void onAlcoholConsumption(PlayerItemConsumeEvent event) {
@@ -48,6 +54,8 @@ public class BarListener implements Listener {
             }
         }
         double amount = playerConsumption.get(playerUUID);
+        Random rand = new Random();
+
         if (amount < 30) {
             return;
         }
@@ -55,19 +63,32 @@ public class BarListener implements Listener {
             // 0-1
             deathChance(player, 2, playerUUID);
             player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 12000, 9));
+//            if (rand.nextInt(5) > 1) {
+//                playerPuke(player);
+//            }
 
         } else if (amount > 90) {
             // 0-3
             deathChance(player, 4, playerUUID);
             player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 12000, 8));
+//            if (rand.nextInt(4) > 1) {
+//                playerPuke(player);
+//            }
         } else if (amount > 80) {
             // 0-5
             deathChance(player, 6, playerUUID);
             player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 12000, 6));
+//            if (rand.nextInt(3) > 1) {
+//                playerPuke(player);
+//            }
+
         } else if (amount > 70) {
             // 0-7
             deathChance(player, 8, playerUUID);
             player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 12000, 4));
+//            if (rand.nextInt(2) == 1) {
+//                playerPuke(player);
+//            }
         } else if (amount > 60) {
             // 0-9
             deathChance(player, 10, playerUUID);
@@ -116,5 +137,24 @@ public class BarListener implements Listener {
         Bukkit.getScheduler().runTaskLater(Core.getInstance(), () -> player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&2t&fBar&7] &cYou blacked out and woke up in a random location.")), 110);
     }
 
-
+//    public void puke(Player player) {
+//        Location loc = player.getLocation();
+//        loc.setY(loc.getY() + 1.1);
+//        loc.setPitch(loc.getPitch() - 10 + pukeRand.nextInt(20));
+//        loc.setYaw(loc.getYaw() - 10 + pukeRand.nextInt(20));
+//        Vector direction = loc.getDirection();
+//        direction.multiply(0.5);
+//        loc.add(direction);
+//        Item item = player.getWorld().dropItem(loc, new ItemStack(Material.SOUL_SAND));
+//        item.setVelocity(direction);
+//        item.setPickupDelay(32767);
+//    }
+//
+//    public void playerPuke(Player player) {
+//        if (count != 0) {
+//            puke(player);
+//            count--;
+//            playerPuke(player);
+//        }
+//    }
 }
