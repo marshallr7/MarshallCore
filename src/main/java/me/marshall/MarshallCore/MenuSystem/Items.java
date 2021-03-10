efc850670f004ae050afddfc88692f76531fd31c
Marshall Patterson <mryanp7@gmail.com>
2021-03-10 12:21:41 -0600

    added mobhunterskull method

--- src/main/java/me/marshall/MarshallCore/MenuSystem/Items.java
+++ src/main/java/me/marshall/MarshallCore/MenuSystem/Items.java
@@ -1,9 +1,13 @@
 package me.marshall.MarshallCore.MenuSystem;
 
+import me.marshall.MarshallCore.SkullCreator;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Items {
 
@@ -13,6 +17,20 @@ public class Items {
         blackStainedGlassPaneMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', " "));
         blackStainedGlassPane.setItemMeta(blackStainedGlassPaneMeta);
         return blackStainedGlassPane;
+    }
+
+    public static ItemStack mobHunterSkull(String url, String name) {
+        String skin = url;
+        ItemStack skull = SkullCreator.itemFromUrl(skin);
+        ItemMeta skullMeta = skull.getItemMeta();
+        skullMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+        List<String> lore = new ArrayList<>();
+        lore.add(ChatColor.translateAlternateColorCodes('&', " "));
+        lore.add(ChatColor.translateAlternateColorCodes('&', "&7Click here to view"));
+        lore.add(ChatColor.translateAlternateColorCodes('&', name + " &7contracts."));
+        skullMeta.setLore(lore);
+        skull.setItemMeta(skullMeta);
+        return skull;
     }
 
 }
