4d7e4c4f1681c3a92214b7a0d39b47eb7b927c6a
Marshall Patterson <mryanp7@gmail.com>
2021-03-10 12:22:04 -0600

    added villager interact method

--- src/main/java/me/marshall/MarshallCore/MobHunter/Listeners/MobHunterListener.java
+++ src/main/java/me/marshall/MarshallCore/MobHunter/Listeners/MobHunterListener.java
@@ -1,6 +1,44 @@
 package me.marshall.MarshallCore.MobHunter.Listeners;
 
+import me.marshall.MarshallCore.Core;
+import me.marshall.MarshallCore.MobHunter.Menus.MobHunterMenu;
+import net.md_5.bungee.api.ChatColor;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class MobHunterListener implements Listener {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @EventHandler
+    public void villagerInteract(PlayerInteractEntityEvent event) {
+        Player player = event.getPlayer();
+        if (!event.getRightClicked().getCustomName().contains(ChatColor.translateAlternateColorCodes('&', "&fMob Hunter"))) {
+            return;
+        }
+        MobHunterMenu menu = new MobHunterMenu(Core.getPlayerMenuUtility(player));
+        menu.open();
+
+
+    }
 }
