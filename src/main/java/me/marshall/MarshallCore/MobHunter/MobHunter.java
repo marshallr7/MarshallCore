54ccdc6d1b632dfa4515392543bcda8a135f939d
Marshall Patterson <mryanp7@gmail.com>
2021-03-13 12:35:42 -0600

    created hashmaps for pre-contract purchase

---
+++ src/main/java/me/marshall/MarshallCore/MobHunter/MobHunter.java
@@ -0,0 +1,10 @@
+package me.marshall.MarshallCore.MobHunter;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class MobHunter {
+
+    public HashMap<UUID, Double> attemptedContractPurchase = new HashMap<>();
+    public HashMap<UUID, String> attemptedContractBoss = new HashMap<>();
+}
