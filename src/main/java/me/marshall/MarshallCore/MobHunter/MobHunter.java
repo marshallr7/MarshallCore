45bb9a6c2e284e8b8f8c17bf5fe02f656f9fb61d
Marshall Patterson <mryanp7@gmail.com>
2021-03-13 16:55:39 -0600

    added getters and setters

--- src/main/java/me/marshall/MarshallCore/MobHunter/MobHunter.java
+++ src/main/java/me/marshall/MarshallCore/MobHunter/MobHunter.java
@@ -7,4 +7,30 @@ public class MobHunter {
 
     public HashMap<UUID, Double> attemptedContractPurchase = new HashMap<>();
     public HashMap<UUID, String> attemptedContractBoss = new HashMap<>();
+
+    public String getAttemptedContractBoss(UUID playerUUID) {
+        return attemptedContractBoss.get(playerUUID);
+    }
+
+    public void setAttemptedContractBoss(UUID playerUUID, String mobName) {
+        if (attemptedContractBoss.containsKey(playerUUID)) {
+            attemptedContractBoss.replace(playerUUID, mobName);
+        } else {
+            attemptedContractBoss.put(playerUUID, mobName);
+        }
+    }
+
+    public Double getAttemptedContractPurchase(UUID playerUUID) {
+        return attemptedContractPurchase.get(playerUUID);
+    }
+
+    public void setAttemptedContractPurchase(UUID playerUUID, Double amount) {
+        if (attemptedContractPurchase.containsKey(playerUUID)) {
+            attemptedContractPurchase.replace(playerUUID, amount);
+        } else {
+            attemptedContractPurchase.put(playerUUID, amount);
+        }
+    }
+
+
 }
