ea261c520b1724fbd72fff1ed70bd0f03dfb94d7
Marshall Patterson <mryanp7@gmail.com>
2021-03-18 19:02:18 -0500

    modifications

--- src/main/java/me/marshall/MarshallCore/MobHunter/Menus/MobMenus/OdinMenu.java
+++ src/main/java/me/marshall/MarshallCore/MobHunter/Menus/MobMenus/OdinMenu.java
@@ -27,8 +27,8 @@ public class OdinMenu extends Menu {
 
     @Override
     public void setMenuItems() {
-        // Once a noble leader, slain by Fenrir, summoned again to reclaim his crown.
-        String[] odinTierOne = {"&f", "&7click here to view", "&8Odin &7contracts."};
+        // incarnated
+        String[] odinTierOne = {"&7Difficulty: Easy", "&f", "&7Health: &cxxxx❤", "&7Damage: &cxx &7", "&f", "&aAbility One", "&7Ability one description", "&f", "&2Reward:", "    &7x Odin Hunter Exp", "    &71x Random Odin Drop"};
         inventory.setItem(11, Items.mobHunterSkull("http://textures.minecraft.net/texture/c70413173349cd10e3e574edbefefcf91f4b4843083925e3082a486ff9769975","&8Odin", odinTierOne));
     }
 }
