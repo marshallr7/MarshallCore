5138d823036b0046632e8994027686fe4e39346c
Marshall Patterson <mryanp7@gmail.com>
2021-03-18 19:02:10 -0500

    optimized imports

--- src/main/java/me/marshall/MarshallCore/MobHunter/Menus/MobHunterMenu.java
+++ src/main/java/me/marshall/MarshallCore/MobHunter/Menus/MobHunterMenu.java
@@ -5,7 +5,6 @@ import me.marshall.MarshallCore.MenuSystem.Items;
 import me.marshall.MarshallCore.MenuSystem.Menu;
 import me.marshall.MarshallCore.MenuSystem.PlayerMenuUtility;
 import me.marshall.MarshallCore.MobHunter.Menus.MobMenus.*;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
