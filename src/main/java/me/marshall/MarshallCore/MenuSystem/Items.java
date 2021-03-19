6ee43d79b2dd345e502fbf696d3ffb2f9008dac2
Marshall Patterson <mryanp7@gmail.com>
2021-03-18 19:01:33 -0500

    added confirm and deny items

--- src/main/java/me/marshall/MarshallCore/MenuSystem/Items.java
+++ src/main/java/me/marshall/MarshallCore/MenuSystem/Items.java
@@ -1,5 +1,6 @@
 package me.marshall.MarshallCore.MenuSystem;
 
+import me.marshall.MarshallCore.MobHunter.MobHunter;
 import me.marshall.MarshallCore.SkullCreator;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -8,6 +9,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Items {
 
@@ -25,7 +27,7 @@ public class Items {
         ItemMeta skullMeta = skull.getItemMeta();
         skullMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
         List<String> lore = new ArrayList<>();
-        for (int i = 0; i <= loreLines.length-1; i++) {
+        for (int i = 0; i <= loreLines.length - 1; i++) {
             lore.add(ChatColor.translateAlternateColorCodes('&', loreLines[i]));
         }
         skullMeta.setLore(lore);
@@ -46,4 +48,30 @@ public class Items {
         return item;
     }
 
+    public static ItemStack confirmItem(UUID playerUUID) {
+        ItemStack item = new ItemStack(Material.LIME_WOOL);
+        ItemMeta itemMeta = item.getItemMeta();
+        itemMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', "&aConfirm"));
+        List<String> lore = new ArrayList<>();
+        lore.add(ChatColor.translateAlternateColorCodes('&', "&f"));
+        lore.add(ChatColor.translateAlternateColorCodes('&', "&7Click here to start the contract."));
+        Integer foundValue = MobHunter.getAttemptedContractPurchase(playerUUID);
+        lore.add(ChatColor.translateAlternateColorCodes('&', "&f"));
+        lore.add(ChatColor.translateAlternateColorCodes('&', "&2Price: $" + foundValue));
+        itemMeta.setLore(lore);
+        item.setItemMeta(itemMeta);
+        return item;
+    }
+
+    public static ItemStack cancelItem() {
+        ItemStack item = new ItemStack(Material.RED_WOOL);
+        ItemMeta itemMeta = item.getItemMeta();
+        itemMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', "&cCancel"));
+        List<String> lore = new ArrayList<>();
+        lore.add(ChatColor.translateAlternateColorCodes('&', "&f"));
+        lore.add(ChatColor.translateAlternateColorCodes('&', "&7Click here to cancel your purchase."));
+        itemMeta.setLore(lore);
+        item.setItemMeta(itemMeta);
+        return item;
+    }
 }
