441e4774b040c186c217c8ded691c618c14124ac
Marshall Patterson <mryanp7@gmail.com>
2021-04-13 00:11:34 -0500

    getGems method

--- src/main/java/me/marshall/MarshallCore/Currency/Gems.java
+++ src/main/java/me/marshall/MarshallCore/Currency/Gems.java
@@ -1,9 +1,12 @@
 package me.marshall.MarshallCore.Currency;
 
+import me.marshall.MarshallCore.Core;
+
 import java.util.HashMap;
 import java.util.UUID;
 
 public class Gems {
+
 
     HashMap<UUID, Integer> playerBalances = new HashMap<>();
 
@@ -17,5 +20,10 @@ public class Gems {
         Integer currentBal = playerBalances.get(playerUUID);
         Integer updatedBal = currentBal - amount;
         playerBalances.replace(playerUUID, updatedBal);
+    }
+
+    public void getGems(UUID playerUUID) {
+        Core plugin = Core.getInstance();
+        int gems = plugin.getPlayerFile(playerUUID).getInt("gems");
     }
 }
