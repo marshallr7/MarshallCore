83d522aa770ab7d2a5df64b57caaf250fd289b2b
Marshall Patterson <mryanp7@gmail.com>
2021-04-13 14:39:20 -0500

    add gems to hashmap on player login

--- src/main/java/me/marshall/MarshallCore/BaseListeners.java
+++ src/main/java/me/marshall/MarshallCore/BaseListeners.java
@@ -1,5 +1,6 @@
 package me.marshall.MarshallCore;
 
+import me.marshall.MarshallCore.Currency.Gems;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,24 +8,32 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public class BaseListeners implements Listener {
 
     Core plugin = Core.getInstance();
 
 
-    //CREATE FILE ON PLAYER LOGIN
     @EventHandler
     public void playerFileCreation(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+        UUID playerUUID = player.getUniqueId();
+
+        //CREATE FILE ON PLAYER LOGIN
         new BukkitRunnable() {
             public void run() {
                 try {
-                    plugin.createCustomConfig(player.getUniqueId(), player.getPlayerListName());
+                    plugin.createCustomConfig(playerUUID, player.getPlayerListName());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }.runTaskAsynchronously(plugin);
+
+        Gems gems = new Gems();
+        int gemsBalance = gems.getGems(playerUUID);
+        Gems.playerBalances.put(playerUUID, gemsBalance);
+
     }
 }
